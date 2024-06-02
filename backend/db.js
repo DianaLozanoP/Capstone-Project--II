682f39002baaf40d060345c7ba8873e6e834260f
dianaloz <dlbogota2104@gmail.com>
2024-06-02 13:28:48 -0400

    Changed db part

--- backend/db.js
+++ backend/db.js
@@ -1,37 +1,59 @@
 "use strict";
 /** Database setup for jobly. */
+// const { Client } = require("pg");
+// const password = require('./password')
+// // const { getDatabaseUri } = require("./config");
+
+// let database;
+// if (process.env.NODE_ENV === "test") {
+//     database = "doculab_test";
+// } else {
+//     database = "doculab";
+// }
+
+// let db;
+// if (process.env.NODE_ENV === "production") {
+//     db = new Client({
+//         ssl: {
+//             rejectUnauthorized: false
+//         }, user: 'dianaloz',
+//         host: '/var/run/postgresql',
+//         database: database,
+//         password: password,
+//         port: 5432
+//     });
+// } else {
+//     db = new Client({
+//         user: 'dianaloz',
+//         host: '/var/run/postgresql',
+//         database: database,
+//         password: password,
+//         port: 5432
+//     });
+// }
+
+// db.connect();
+
+// module.exports = db;
+
 const { Client } = require("pg");
-const password = require('./password')
-// const { getDatabaseUri } = require("./config");
+const connectionString = "postgres://yjphmgsv:Tap5iSfhwSFb5_qMb0GmJAMq5X7H90LN@bubble.db.elephantsql.com/yjphmgsv";
 
-let database;
-if (process.env.NODE_ENV === "test") {
-    database = "doculab_test";
-} else {
-    database = "doculab";
-}
+const db = new Client({
+    connectionString: connectionString
+});
 
-let db;
-if (process.env.NODE_ENV === "production") {
-    db = new Client({
-        ssl: {
-            rejectUnauthorized: false
-        }, user: 'dianaloz',
-        host: '/var/run/postgresql',
-        database: database,
-        password: password,
-        port: 5432
+db.connect()
+    .then(() => {
+        console.log('Connected to the database');
+        // Perform database operations here
+    })
+    .catch(err => {
+        console.error('Error connecting to the database:', err);
+        // Additional error details for debugging
+        console.error('Error code:', err.code); // PostgreSQL error code
+        console.error('Error message:', err.message); // Description of the error
+        console.error('Error stack:', err.stack); // Stack trace
     });
-} else {
-    db = new Client({
-        user: 'dianaloz',
-        host: '/var/run/postgresql',
-        database: database,
-        password: password,
-        port: 5432
-    });
-}
-
-db.connect();
 
 module.exports = db;
